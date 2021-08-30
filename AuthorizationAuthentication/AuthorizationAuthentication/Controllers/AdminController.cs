f51884999655ada03b547a4df0ab52c18d1ca3cb
KudryavtsevAlexey <a.kudryavcev0812@bk.ru>
2021-08-30 20:00:23 +0300

    Добавьте файлы проекта.

---
+++ AuthorizationAuthentication/AuthorizationAuthentication/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthorizationAuthentication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthorizationAuthentication.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            return View();
+        }
+
+        [AllowAnonymous, HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            return View();
+        }
+    }
+}
